98982244c7b61f3c78ec16916c5e6bc838172733
EliezerAmbion <eliezerambion@gmail.com>
2022-12-26 15:33:14 +0800

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+// you need to import this to use the provider
+import 'package:provider/provider.dart';
 
-import 'screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import 'providers/products_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,16 +13,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple).copyWith(
-          secondary: Colors.deepOrange,
+    // ChangeNotifierProvider allows us to register a class to which you can then listen in child widgets.
+    // And whenever that class updates the widgets which are listening (child widgets here) will rebuild.
+    return ChangeNotifierProvider(
+      // The builder method will receive a context,
+      // and it will return a new instance of you provider class, in this case, the ProductsProvider class.
+      create: (context) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+
+  // final String title;
+  // final String price;
+
+  // const ProductDetailScreen({
+  //   super.key,
+  //   required this.title,
+  //   required this.price,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
